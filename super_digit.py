42537bf0382e0b402d245a64e94939e1c02f322d
joaogdfaero <joao.dal@acad.ufsm.br>
2022-11-09 16:10:00 -0300

    super digit fazendo

---
+++ super_digit.py
@@ -0,0 +1,29 @@
+# https://www.hackerrank.com/challenges/one-week-preparation-kit-recursive-digit-sum/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=one-week-preparation-kit&playlist_slugs%5B%5D=one-week-day-four
+
+def superDigit(n, k):
+    p = n*k
+    
+    print(p)
+    # convert p para int
+
+    # soma todos os digitos de p
+    soma = 0
+    for i in range(0,len(p)-1):
+        print(soma)
+        print('int(p[i])')
+        print(int(p[i]))
+        print('p[i+1]')
+        int(int(p[i+1]))
+
+        soma = soma + int(p[i])+int(p[i+1])
+
+    print(soma)
+
+
+
+
+
+n = '148'
+k = 3
+
+superDigit(n,k)
