c189ef8dbb99f13327bea9e56e27011927160516
joaogdfaero <joao.dal@acad.ufsm.br>
2022-11-09 15:57:38 -0300

    desisti do no prefix set

---
+++ no_prefix_set.py
@@ -0,0 +1,11 @@
+#hackerrank.com/challenges/no-prefix-set/problem # NÃO CONSEGUI RESOLVER
+def noPrefix(words):
+    for i in range(0,len(words) - 1):
+        if words[i] in words.remove(words[i]):
+            print(words)
+            print("true")
+    
+
+
+words = ['aab', 'defgab', 'abcde', 'aabcde', 'bbbbbbbbbb', 'jabjjjad']
+noPrefix(words)
